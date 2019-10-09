e055248e4bc6fc56e3368f1020eacd0dc57fc8a2
MarioMatheus <mariomatheu@gmail.com>
2019-10-09 00:46:11 -0300

    Added live reload to webpack and overriding console log to future logs

--- src/main.js
+++ src/main.js
@@ -1,42 +1,28 @@
-// const { inspect } = require('util')
-
 const Lexer = require('./compiler/lexer')
 const Parser = require('./compiler/parser')
 const SemanticAnalyser = require('./compiler/semantic')
 const CodeGenerator = require('./compiler/code-generator')
 
-const ragnakode = `
-  ragna: STR 5
-  ragna: MUL ragna 2
-  kode: DIV ragna 10
-  kode: PRT
-`
-
-window.compileAndRun = function() {
-  const code = document.querySelector('#editor').value
-
-  console.log('\nTOKENIZING')
-  const tokens = Lexer.tokenize(code)
-  // console.log(tokens)
-
-  console.log('\nPARSING')
-  const { ast, symbolTable } = Parser.parse(tokens)
-  // console.log(inspect(ast, {showHidden: false, depth: null}))
-
-  console.log('\nAnalysing')
-  SemanticAnalyser.analyze(ast, symbolTable)
-
-  console.log('\nJS CODE')
-  const outputFileData = CodeGenerator.generate(ast, symbolTable)
-  // console.log(outputFileData)
-
-  console.log('\nCODE EXECUTION OUTPUT')
-  eval(outputFileData)
+console.oldLog = console.log
+console.log = function(value) {
+  console.oldLog(value)
+  return value
 }
 
-// const tokens = Lexer.tokenize(ragnakode)
-// const { ast, symbolTable } = Parser.parse(tokens)
-// console.log(symbolTable)
-// SemanticAnalyser.analyze(ast, symbolTable)
-// const compiledProgram = CodeGenerator.generate(ast, symbolTable)
-// console.log(/*inspect(*/compiledProgram/*, true, null, true)*/)
+const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+  lineNumbers: true,
+  mode: 'ragnakode',
+  lineWrapping: true
+})
+
+window.compileAndRun = function() {
+  const code = editor.getValue()
+
+  const tokens = Lexer.tokenize(code)
+  const { ast, symbolTable } = Parser.parse(tokens)
+  SemanticAnalyser.analyze(ast, symbolTable)
+  const outputFileData = CodeGenerator.generate(ast, symbolTable)
+
+  const output = eval(outputFileData)
+  console.log(typeof output)
+}
