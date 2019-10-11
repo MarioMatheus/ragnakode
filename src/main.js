9376f039d7545f45865cd4b83c7195e5c5134e10
MarioMatheus <mariomatheu@gmail.com>
2019-10-11 15:49:23 -0300

    bug fixed - console doesnt print many values

--- src/main.js
+++ src/main.js
@@ -3,36 +3,37 @@ const Parser = require('./compiler/parser')
 const SemanticAnalyser = require('./compiler/semantic')
 const CodeGenerator = require('./compiler/code-generator')
 
-console.oldLog = console.log
-console.log = function(value) {
-  console.oldLog(value)
-  return value
-}
-
+const consoleElement = document.querySelector('#console')
 const editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
   lineNumbers: true,
   mode: 'ragnakode',
   lineWrapping: true
 })
 
+console.oldLog = console.log
+console.log = function(value) {
+  console.oldLog(value)
+  consoleElement.value += `${value}\n`
+}
+
 function compile(code) {
-  let output = ''
   try {
     const tokens = Lexer.tokenize(code)
-    console.oldLog(tokens)
     const { ast, symbolTable } = Parser.parse(tokens)
     SemanticAnalyser.analyze(ast, symbolTable)
     const outputFileData = CodeGenerator.generate(ast, symbolTable)
-
-    output = eval(outputFileData)
+    eval(outputFileData)
   } catch (error) {
-    output = console.log(error)
+    console.log(error)
   }
-  return output
 }
 
 window.compileAndRun = function() {
   const code = editor.getValue()
-  output = (!code || code.length === 0) ? 'Type something' : compile(code)
-  document.querySelector('#console').value = output
+  consoleElement.value = ''
+  if (!code || code.length === 0) {
+    consoleElement.value = 'Type something'
+  } else {
+    compile(code)
+  }
 }
